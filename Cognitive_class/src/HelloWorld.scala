037ab1d76d9254d30474a54b1af50f6f6bc189e1
user <user@192.168.0.150>
2018-03-22 12:31:40 +0100

    Rebuilt as Maven project

--- Cognitive_class/src/HelloWorld.scala
+++ Cognitive_class/src/HelloWorld.scala
@@ -1,5 +1,9 @@
 
 
-class HelloWorld {
+object HelloWorld extends App {
+  val hello = "Hello"
+  def echo(message: String): Unit = println(message * 2)
   println("Hello world!")
+  
+  echo("I am smart! ")
 }
