3bb12d9f1c7c665fc3c949fa87d112434bcab2db
user <user@192.168.0.150>
2018-04-08 22:03:54 +0200

    SumOfSquares added

--- com.baczko/src/main/java/HelloScala.scala
+++ com.baczko/src/main/java/HelloScala.scala
@@ -1,5 +1,12 @@
 object HelloScala {
   def main(args: Array[String]){
     println("Hello, Scala!")
+    
+    "Hello".intersect("World")
+    "Bonjour".sorted
+    
+    val s = "Hello"
+    
+    s(4)
   } 
 }
--- com.baczko/src/main/java/recursions/simple_math.scala
+++ com.baczko/src/main/java/recursions/simple_math.scala
@@ -8,4 +8,10 @@ object simple_math {
     
   def factorial (a: Int) : Int =
     if (a == 0) 1 else a * factorial(a - 1)
+    
+  def square (a: Int) : Int = a * a   
+    
+  //sum of squares of int between a and b
+  def sumOfSquares (a: Int, b: Int) : Int =
+    if (a > b) 0 else square(a) + sumOfSquares(a + 1, b) 
 }
