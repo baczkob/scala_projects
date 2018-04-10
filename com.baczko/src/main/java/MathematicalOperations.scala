de1e4861430394edb49da00d46df86cd5d3c6418
baczkob <baczkopolter@gmail.com>
2018-04-10 13:32:40 +0200

    added sumAny

--- com.baczko/src/main/java/MathematicalOperations.scala
+++ com.baczko/src/main/java/MathematicalOperations.scala
@@ -1,6 +1,13 @@
 
 object MathematicalOperations extends App {
   
+   //returns sum of any number of components
+  def sumAny(args: Int*){
+  var result = 0
+  for (arg <- args) result += arg
+  result
+}
+
   import recursions.simple_math._
   println(gcd(14, 28))
   println(factorial(5))
